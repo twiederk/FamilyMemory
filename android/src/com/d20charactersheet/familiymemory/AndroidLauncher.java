faea37b823893999b0d9efbf988a967371f5671e
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2019-09-11 21:45:22 +0200

    Added android module. Upgrade gradle to 5.4.1

---
+++ android/src/com/d20charactersheet/familiymemory/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.d20charactersheet.familiymemory;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.d20charactersheet.familiymemory.FamilyMemoryMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FamilyMemoryMain(), config);
+	}
+}
